27ff7c47bef8c77ebc1e28a9c7f11ae785def242
ondrejstasek <61665409+ondrejstasek@users.noreply.github.com>
2023-02-03 12:38:30 +0100

    formula

---
+++ Formula/xccov-json-to-sonar-xml.rb
@@ -0,0 +1,16 @@
+class XccovJsonToSonarXml < Formula
+    desc "Command-line tool to convert Xcode code coverage to SonarQube generic format"
+    homepage "https://github.com/ondrejstasek/xccov-json-to-sonar-xml/"
+    url "https://github.com/ondrejstasek/xccov-json-to-sonar-xml.git", :tag => "1.2.0"
+  
+    depends_on :xcode => ["11.0", :build]
+  
+    def install
+      bin.mkpath
+      system "make", "install", "clean", "prefix=#{prefix}"
+    end
+  
+    test do
+      system "#{bin}/xccov-json-to-sonar-xml" "-h"
+    end
+  end
